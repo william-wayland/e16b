ff192272f0a08af5cae497e1c97ce56a123a5a23
William Wayland <william.wayland@griffithuni.edu.au>
2019-08-05 20:34:56 +1000

    Removed tabs, looks nicer

--- main.cpp
+++ main.cpp
@@ -24,67 +24,67 @@ word flags;
 // instructions
 #define NOP 0x00
 #define LDA 0x01 // Load Value from RAM into regA
-#define	STA	0x02 // Store Value from regA into RAM
-#define	ADD	0x03 // Load value from ram and add to regA
-#define	SUB	0x04
+#define STA 0x02 // Store Value from regA into RAM
+#define ADD 0x03 // Load value from ram and add to regA
+#define SUB 0x04 
 
-#define	OUT	0x05
+#define OUT 0x05
 
-#define	JMP	0x06
-#define	JC	0x07
-#define	JZ	0x08
+#define JMP 0x06
+#define JC  0x07
+#define JZ  0x08
 
 #define HLT 0x0a
 
-#define	LDI	0x10 // Load instantly
-#define	ADI	0x11 // Add  instantly
+#define LDI 0x10 // Load instantly
+#define ADI 0x11 // Add  instantly
 
-#define	LDR	0x20 // Load Value from ROM into regA
-#define	ADR	0x21 // Add value from ROM into regA
+#define LDR 0x20 // Load Value from ROM into regA
+#define ADR 0x21 // Add value from ROM into regA
 
 class ALU {
 public:
-	void invoke() {
-		word in = bus;
-		regA = add(in, regA);
-	}
+  void invoke() {
+    word in = bus;
+    regA = add(in, regA);
+  }
 private:
-	word add(word a, word b) {
+  word add(word a, word b) {
     flags &= ~(ZERO_FLAG | CARRY_FLAG); // clear flags
 
     // If the addtion will result in a overflow
-		if (a > 0 && b > 0xFFFF - a) {
-			flags |= CARRY_FLAG;
-		}
+    if (a > 0 && b > 0xFFFF - a) {
+      flags |= CARRY_FLAG;
+    }
 
-		word temp = a + b;
-		if (temp == 0) {
-			flags |= ZERO_FLAG;
-		}
-		return temp;
-	}
+    word temp = a + b;
+    if (temp == 0) {
+      flags |= ZERO_FLAG;
+    }
+    return temp;
+  }
 } Alu;
 
 class PROGRAMCOUNTER {
 public:
-	void invoke() {
+  void invoke() {
     if ((flags & JUMP_FLAG) == JUMP_FLAG) {
       count = bus;
       flags &= ~JUMP_FLAG;
     } else {
       count += 1;
     }
-		
-	}
-	void reset() {
-		count = 0;
-	}
-	word count = 0;
+  
+  }
+  void reset() {
+    count = 0;
+  }
+  word count = 0;
 } ProgramCounter;
 
 class R0M {
 public:
-	std::array<word, 128> storage = {};
+  std::array<word, 128> storage = {};
 
   int readNumber(std::stringstream &ss){
     int x;
@@ -180,76 +180,74 @@ private:
 
 class RAM {
 public:
-	std::array<word, 128> storage;
+  std::array<word, 128> storage;
 } Ram;
 
 int main() {
   Rom.loadRom();
-	while (true) {
-		auto currentInstructionFull = Rom.storage[ProgramCounter.count];
-		auto instruction = (word)(currentInstructionFull >> 0x8);
-		auto location = (word)(currentInstructionFull & 0x00FF);
-	
-		switch (instruction) {
-		case NOP:
-			break;
+  while (true) {
+    auto currentInstructionFull = Rom.storage[ProgramCounter.count];
+    auto instruction = (word)(currentInstructionFull >> 0x8);
+    auto location = (word)(currentInstructionFull & 0x00FF);
+  
+    switch (instruction) {
+    case NOP:
+      break;
 
-		// Loads ram location into regA
-		case LDA: 
-			regA = Ram.storage[location];
-			break;
+    // Loads ram location into regA
+    case LDA: 
+      regA = Ram.storage[location];
+      break;
     // Loads rom location into regA
-		case LDR:
-			regA = Rom.storage[location];
-			break;
+    case LDR:
+      regA = Rom.storage[location];
+      break;
     // Loads immediate value (max half of LDA and LDR)
-		case LDI:
-			regA = location;
-			break;
+    case LDI:
+      regA = location;
+      break;
 
-		// Stores regA into ram 
-		case STA:
-			Ram.storage[location] = regA;
-			break;
+    // Stores regA into ram 
+    case STA:
+      Ram.storage[location] = regA;
+      break;
 
-		// Adds ram value to regA
-		case ADD:
-			bus = Ram.storage[location];
-			Alu.invoke();
-			break;
+    // Adds ram value to regA
+    case ADD:
+      bus = Ram.storage[location];
+      Alu.invoke();
+      break;
     // Adds immediate value to regA
-		case ADI:
-			bus = location;
-			Alu.invoke();
-			break;
+    case ADI:
+      bus = location;
+      Alu.invoke();
+      break;
 
-		case OUT:
-			std::cout << std::dec << (int)regA << std::endl;
-			break;
+    case OUT:
+      std::cout << std::dec << (int)regA << std::endl;
+      break;
 
-		case JMP:
-			bus = location;
+    case JMP:
+      bus = location;
       flags |= JUMP_FLAG;
-			break;
-		case JC:
-			if ((flags & CARRY_FLAG) == CARRY_FLAG) {
-				bus = location;
+      break;
+    case JC:
+      if ((flags & CARRY_FLAG) == CARRY_FLAG) {
+        bus = location;
         flags |= JUMP_FLAG;
-			}
-			break;
-		case JZ:
-			if ((flags & ZERO_FLAG) == ZERO_FLAG) {
-				bus = location;
+      }
+      break;
+    case JZ:
+      if ((flags & ZERO_FLAG) == ZERO_FLAG) {
+        bus = location;
         flags |= JUMP_FLAG;
-			}
-			break;
-			
-		case HLT:
-			while (1);
-			break;
-		}
-		ProgramCounter.invoke();
-	}
-	
-	while (1);
+      }
+      break;
+      
+    case HLT:
+      while (1);
+      break;
+    }
+    ProgramCounter.invoke();
+  }
 }
